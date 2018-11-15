21bbc9cee9c7a0606c51a28085bb9318dd361482
Ivo Lejon <ivo.lejon@gmail.com>
2018-11-15 22:38:11 +0100

    ..

--- app.py
+++ app.py
@@ -29,6 +29,11 @@ def readFromRedis():
 def index():
     return 'Lightsail test'
 
+@app.route("/write", methods=['GET'])
+def writeToRedis():
+    r = redis.StrictRedis(host='redis', port=6379, db=0)
+    r.set('foo', 'bar')
+    
 @app.route("/testo", methods=['GET'])
 def simulate():
     try:
@@ -45,10 +50,7 @@ if __name__ == "__main__":
     app.debug = True
     app.run(debug=True, host='0.0.0.0', port=5000)
 
-@app.route("/write", methods=['GET'])
-def writeToRedis():
-    r = redis.StrictRedis(host='redis', port=6379, db=0)
-    r.set('foo', 'bar')
+
 
 
 
