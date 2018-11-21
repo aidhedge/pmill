e01e2d9462d79bd132e544868b76330a4ec7c23d
Ivo Lejon <ivo.lejon@gmail.com>
2018-11-21 16:56:22 +0100

    added notebook-json return

--- pmill.py
+++ pmill.py
@@ -1,6 +1,10 @@
 import papermill as pm
 import json
 import uuid
+import boto3
+import os
+
+
 
 class Pmill(object):
 
@@ -9,6 +13,7 @@ class Pmill(object):
         self.uuid = '{}-{}'.format(nbInputFileName, str(uuid.uuid1()))
         self.parameters = parameters
         self.nbInputFileName = nbInputFileName
+        self.notebook = None
         # self.nb_data = None
 
     def executeNotebook(self):
@@ -21,9 +26,26 @@ class Pmill(object):
         report_mode=False
         )
 
+    def getS3(self):
+        s3 = boto3.resource(
+        's3', 
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID', default=None),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', default=None)
+        )
+
+        filepath = 'output/'+self.uuid+'.ipynb'
+        print(filepath)
+        content_object = s3.Object('ah-papermill', filepath)
+        file_content = content_object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        return json_content
+
     def getOutput(self):
-        with open('output.ipynb') as f:
-            data = json.load(f)
+        output = dict()
+        data = self.getS3()
+        output.update(nb = data)
+        # with open('output.ipynb') as f:
+        #     data = json.load(f)
 
         output_cell = [d for d in data["cells"] if d['metadata']['tags'] == ['calculation_result']][0]
         outputs = output_cell['outputs']
@@ -31,8 +53,9 @@ class Pmill(object):
         for out in outputs:
             out['data']['text/plain'][0] = out['data']['text/plain'][0].replace("'","\"")
             self.result.append(json.loads(out['data']['text/plain'][0]))
-
-        return json.dumps(self.result)
+        output.update(result = self.result)
+        
+        return json.dumps(output)
 
 
 
